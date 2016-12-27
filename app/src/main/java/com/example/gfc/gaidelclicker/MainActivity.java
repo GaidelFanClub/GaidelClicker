5312ef37ae8ba729e340478ea5ee50300a627696
Артемий <akiberq@mail.ru>
2016-12-28 00:14:10 +0300

    new icons

--- app/src/main/java/com/example/gfc/gaidelclicker/MainActivity.java
+++ app/src/main/java/com/example/gfc/gaidelclicker/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.gfc.gaidelclicker;
 
 import android.animation.ObjectAnimator;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
