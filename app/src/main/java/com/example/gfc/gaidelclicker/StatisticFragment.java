f75dd9705cf18a77e91d5b13630302c9721244c5
Артемий <akiberq@mail.ru>
2017-02-21 23:56:58 +0400

    New skin

--- app/src/main/java/com/example/gfc/gaidelclicker/StatisticFragment.java
+++ app/src/main/java/com/example/gfc/gaidelclicker/StatisticFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.gfc.gaidelclicker.stat.StatRepository;
 import com.example.gfc.gaidelclicker.stat.StatisticAdapter;
 
 import java.math.BigDecimal;
